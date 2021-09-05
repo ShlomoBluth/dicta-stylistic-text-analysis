ada7b90422467361b916e5cc1142721ce91e1b2c
Shlomo Bluth <lomibluth@gmail.com>
2021-09-05 14:07:41 +0300

    addd commit

--- cypress/integration/requestsTests.js
+++ cypress/integration/requestsTests.js
@@ -2,148 +2,157 @@
 
 //run tests on requests from bible classification run
 
-let sizes = [[1000, 660]]//'iphone-x',
+const urls = new Map();
+urls.set('live',Cypress.env('LIVE_URL'))
+//urls.set('dev',Cypress.env('DEV_URL')) 
+
+const sizes= new Map();
+sizes.set('desktop',[1000, 660])
+//sizes.set('mobile','iphone-x')
 
 
+urls.forEach((urlValue,urlKey)=>{
 
-sizes.forEach((size) => {
+  sizes.forEach((sizeValue,sizeKey) => {
 
-  describe('requestsTests',()=>{
+
+    describe('requestsTests '+urlKey+' '+sizeKey,()=>{
 
     
-    beforeEach(() => {
-      cy.screenSize({size:size})
-      cy.visitpage({url:'/'})
-    })
-  
-  
-    
-  
-  
-    // it('Error message for GetTextLargeAndSmall response with a delay of 5 minutes when clicking the run button'+
-    // ' of tiberias page',()=>{
-    //   cy.selectTextRequest({
-    //     type:'tanach',
-    //     url:'GetTextLargeAndSmall',
-    //     message:' Server took too long to respond.',
-    //     delaySeconds:65*5
-    //   })
-    // })
-  
-    
-    // it('Error message for GetTextLargeAndSmall response with status code 500 when clicking the run button of tiberias page'
-    // ,()=>{
-    //   cy.selectTextRequest({
-    //     type:'tanach',
-    //     url:'GetTextLargeAndSmall',
-    //     status:500,
-    //     message:' Server failed to respond.'
-    //   })
-    // })
-  
+      beforeEach(() => {
+        cy.screenSize({size:sizeValue})
+        cy.visitpage({url:urlValue})
+      })
+
+      // it('Error message for GetTextLargeAndSmall response with a delay of 5 minutes when clicking the run button'+
+      // ' of tiberias page',()=>{
+      //   cy.selectTextRequest({
+      //     type:'tanach',
+      //     url:'GetTextLargeAndSmall',
+      //     message:' Server took too long to respond.',
+      //     delaySeconds:65*5
+      //   })
+      // })
     
       
-  
-    it('Error message for statistics response with a delay of 2 minutes when clicking the run button'+
-    ' of tiberias page',()=>{
-      cy.classificationOfTextRequest({
-        url:'statistics',
-        message:'Feature Extraction: Server took too long to respond.',
-        delaySeconds:65*2
-      })
-    })
-  
+      // it('Error message for GetTextLargeAndSmall response with status code 500 when clicking the run button of tiberias page'
+      // ,()=>{
+      //   cy.selectTextRequest({
+      //     type:'tanach',
+      //     url:'GetTextLargeAndSmall',
+      //     status:500,
+      //     message:' Server failed to respond.'
+      //   })
+      // })
     
-    it('Error message for statistics response with status code 500 when clicking the run button of tiberias page'
-    ,()=>{
-      cy.classificationOfTextRequest({
-        url:'statistics',
-        status:500,
-        message:'Feature Extraction: Server failed to respond.'
-      })
-    })
-  
-    it('Error message for crossvalidate response with a delay of 1 minutes when clicking the run button'+
-    ' of tiberias page',()=>{
-      cy.classificationOfTextRequest({
-        url:'crossvalidate',
-        message:'Cross-validation: Server took too long to respond.',
-        delaySeconds:65*2
-      })
-    })
-  
-    
-    it('Error message for crossvalidate response with status code 500 when clicking the run button of tiberias page'
-    ,()=>{
-      cy.classificationOfTextRequest({
-        url:'crossvalidate',
-        status:500,
-        message:'Cross-validation: Server failed to respond.'
-      })
-    })
-  
-    
-  
-    it('Error message for classify response with a delay of 5 minutes when clicking the run button'+
-    ' of tiberias page',()=>{
-      cy.classificationOfTextRequest({
-        url:'classify',
-        message:'Server took too long to respond.',
-        delaySeconds:65*5
-      })
-    })
-  
-    
-    it('Error message for classify response with status code 500 when clicking the run button of tiberias page'
-    ,()=>{
-      cy.classificationOfTextRequest({
-        url:'classify',
-        status:500,
-        message:'Server failed to respond.'
-      })
-    })
-
-    // it('Error message for UploadFile response with a delay of 5 minutes when clicking the run button'+
-    // ' of tiberias page',()=>{
-    //   cy.selectTextRequest({
-    //     type:'file',
-    //     url:'UploadFile',
-    //     message:'Server took too long to respond.',
-    //     delaySeconds:65*5
-    //   })
-    // })
-  
-    
-    // it('Error message for UploadFile response with status code 500 when clicking the run button of tiberias page'
-    // ,()=>{
-    //   cy.selectTextRequest({
-    //     type:'file',
-    //     url:'UploadFile',
-    //     status:500,
-    //     message:'Server failed to respond.'
-    //   })
-    // })
-
-    // it('Error message for Login response with a delay of 5 minutes when clicking the run button'+
-    // ' of tiberias page',()=>{
-    //   cy.selectTextRequest({
-    //     type:'file',
-    //     url:'Login',
-    //     message:'Server took too long to respond.',
-    //     delaySeconds:65*5
-    //   })
-    // })
-  
-    
-    // it('Error message for Login response with status code 500 when clicking the run button of tiberias page'
-    // ,()=>{
-    //   cy.selectTextRequest({
-    //     type:'file',
-    //     url:'Login',
-    //     status:500,
-    //     message:'Server failed to respond.'
-    //   })
-    // })
       
+        
+    
+      it('Error message for statistics response with a delay of 2 minutes when clicking the run button'+
+      ' of tiberias page',()=>{
+        cy.classificationOfTextRequest({
+          url:'statistics',
+          message:'Feature Extraction: Server took too long to respond.',
+          delaySeconds:65*2
+        })
+      })
+    
+      
+      it('Error message for statistics response with status code 500 when clicking the run button of tiberias page'
+      ,()=>{
+        cy.classificationOfTextRequest({
+          url:'statistics',
+          status:500,
+          message:'Feature Extraction: Server failed to respond.'
+        })
+      })
+    
+      it('Error message for crossvalidate response with a delay of 1 minutes when clicking the run button'+
+      ' of tiberias page',()=>{
+        cy.classificationOfTextRequest({
+          url:'crossvalidate',
+          message:'Cross-validation: Server took too long to respond.',
+          delaySeconds:65*2
+        })
+      })
+    
+      
+      it('Error message for crossvalidate response with status code 500 when clicking the run button of tiberias page'
+      ,()=>{
+        cy.classificationOfTextRequest({
+          url:'crossvalidate',
+          status:500,
+          message:'Cross-validation: Server failed to respond.'
+        })
+      })
+    
+      
+    
+      it('Error message for classify response with a delay of 5 minutes when clicking the run button'+
+      ' of tiberias page',()=>{
+        cy.classificationOfTextRequest({
+          url:'classify',
+          message:'Server took too long to respond.',
+          delaySeconds:65*5
+        })
+      })
+    
+      
+      it('Error message for classify response with status code 500 when clicking the run button of tiberias page'
+      ,()=>{
+        cy.classificationOfTextRequest({
+          url:'classify',
+          status:500,
+          message:'Server failed to respond.'
+        })
+      })
+  
+      // it('Error message for UploadFile response with a delay of 5 minutes when clicking the run button'+
+      // ' of tiberias page',()=>{
+      //   cy.selectTextRequest({
+      //     type:'file',
+      //     url:'UploadFile',
+      //     message:'Server took too long to respond.',
+      //     delaySeconds:65*5
+      //   })
+      // })
+    
+      
+      // it('Error message for UploadFile response with status code 500 when clicking the run button of tiberias page'
+      // ,()=>{
+      //   cy.selectTextRequest({
+      //     type:'file',
+      //     url:'UploadFile',
+      //     status:500,
+      //     message:'Server failed to respond.'
+      //   })
+      // })
+  
+      // it('Error message for Login response with a delay of 5 minutes when clicking the run button'+
+      // ' of tiberias page',()=>{
+      //   cy.selectTextRequest({
+      //     type:'file',
+      //     url:'Login',
+      //     message:'Server took too long to respond.',
+      //     delaySeconds:65*5
+      //   })
+      // })
+    
+      
+      // it('Error message for Login response with status code 500 when clicking the run button of tiberias page'
+      // ,()=>{
+      //   cy.selectTextRequest({
+      //     type:'file',
+      //     url:'Login',
+      //     status:500,
+      //     message:'Server failed to respond.'
+      //   })
+      // })
+  
+  
+    
+  
+  
+    })     
   })
 })
--- cypress/integration/toolTests.js
+++ cypress/integration/toolTests.js
@@ -4,76 +4,85 @@
 
 //run basic tests on bible classification
 
-let sizes = [[1000, 660]]//'iphone-x',
+const urls = new Map();
+urls.set('live',Cypress.env('LIVE_URL'))
+//urls.set('dev',Cypress.env('DEV_URL')) 
+
+const sizes= new Map();
+sizes.set('desktop',[1000, 660])
+//sizes.set('mobile','iphone-x')
 
 
+urls.forEach((urlValue,urlKey)=>{
 
-sizes.forEach((size) => {
+    sizes.forEach((sizeValue,sizeKey) => {
 
-    describe('basicTests',()=>{
-
-        beforeEach(() => {
-            cy.screenSize({size:size})
-            cy.visitpage({url:'/'})
-        })
     
-        it('Run tanach',()=>{
-            cy.get('button').contains('Start Experiment').click()
-            cy.selectTanachText({
-                collection:'Torah',
-                book:'Genesis'
+        describe('toolTests '+urlKey+' '+sizeKey,()=>{
+    
+            beforeEach(() => {
+                cy.screenSize({size:sizeValue})
+                cy.visitpage({url:urlValue})
             })
-            cy.selectTanachText({
-                collection:'Torah',
-                book:'Exodus'
-            })
-            cy.get('button').contains('Analyze Classes').click({force: true})
-            cy.get(':nth-child(1) > .v-spinner > .v-clip',{timeout:1000*60}).should('not.exist')
-            cy.get('button[data-target="#text_for_classification_popup"]').click({force: true})
-            cy.selectTanachText({
-                collection:'Torah',
-                book:'Numbers',
-                popupInner:'true'
-            })
-            cy.get('button').contains('Classify Text').click({force: true})
-            cy.get(':nth-child(13) > .bar').should('have.css','background-color','rgb(204, 0, 193)')
-        })
-
-
-
-        it('Run files',()=>{
-            cy.get('button').contains('Start Experiment').click({force:true})
-            cy.url().should('eq','https://generalclassify.dicta.org.il/#/experiment')
-            cy.selectUploudfile({file:'Genesis.txt'})
-            cy.waitForUploading()
-            cy.selectUploudfile({file:'Exodus.txt'})
-            cy.waitForUploading()
-            cy.get('button').contains('Analyze Classes').click({force: true})
-            cy.get(':nth-child(1) > .v-spinner > .v-clip',{timeout:1000*60}).should('not.exist')
-            cy.get('button[data-target="#text_for_classification_popup"]').click({force: true})
-            cy.selectUploudfile({
-                file:'Leviticus.txt',
-                popupInner:true
-            })
-            cy.waitForUploading()
-            cy.get('button').contains('Classify Text').click({force: true})
-            cy.get(':nth-child(9) > .bar').should('have.css','background-color','rgb(204, 0, 193)')
-        
             
-            // cy.get('button').contains('Analyze Classes').click({force: true})
-
-            // cy.get(':nth-child(1) > .v-spinner > .v-clip',{timeout:1000*60}).should('not.exist')
-            // cy.get('button[data-target="#text_for_classification_popup"]').click({force: true})
-            // cy.selectText({
-            //     collection:'Torah',
-            //     book:'Numbers',
-            //     popupInner:'true'
-            // })
-            // cy.get('button').contains('Classify Text').click({force: true})
-            // cy.get(':nth-child(13) > .bar').should('have.css','background-color','rgb(204, 0, 193)')
-        })
+            it('Run tanach',()=>{
+                cy.get('button').contains('Start Experiment').click()
+                cy.selectTanachText({
+                    collection:'Torah',
+                    book:'Genesis'
+                })
+                cy.selectTanachText({
+                    collection:'Torah',
+                    book:'Exodus'
+                })
+                cy.get('button').contains('Analyze Classes').click({force: true})
+                cy.get(':nth-child(1) > .v-spinner > .v-clip',{timeout:1000*60}).should('not.exist')
+                cy.get('button[data-target="#text_for_classification_popup"]').click({force: true})
+                cy.selectTanachText({
+                    collection:'Torah',
+                    book:'Numbers',
+                    popupInner:'true'
+                })
+                cy.get('button').contains('Classify Text').click({force: true})
+                cy.get(':nth-child(13) > .bar').should('have.css','background-color','rgb(204, 0, 193)')
+            })
     
+    
+    
+            it('Run files',()=>{
+                cy.get('button').contains('Start Experiment').click({force:true})
+                cy.url().should('eq','https://generalclassify.dicta.org.il/#/experiment')
+                cy.selectUploudfile({file:'Genesis.txt'})
+                cy.waitForUploading()
+                cy.selectUploudfile({file:'Exodus.txt'})
+                cy.waitForUploading()
+                cy.get('button').contains('Analyze Classes').click({force: true})
+                cy.get(':nth-child(1) > .v-spinner > .v-clip',{timeout:1000*60}).should('not.exist')
+                cy.get('button[data-target="#text_for_classification_popup"]').click({force: true})
+                cy.selectUploudfile({
+                    file:'Leviticus.txt',
+                    popupInner:true
+                })
+                cy.waitForUploading()
+                cy.get('button').contains('Classify Text').click({force: true})
+                cy.get(':nth-child(9) > .bar').should('have.css','background-color','rgb(204, 0, 193)')
+            
+                
+                // cy.get('button').contains('Analyze Classes').click({force: true})
+    
+                // cy.get(':nth-child(1) > .v-spinner > .v-clip',{timeout:1000*60}).should('not.exist')
+                // cy.get('button[data-target="#text_for_classification_popup"]').click({force: true})
+                // cy.selectText({
+                //     collection:'Torah',
+                //     book:'Numbers',
+                //     popupInner:'true'
+                // })
+                // cy.get('button').contains('Classify Text').click({force: true})
+                // cy.get(':nth-child(13) > .bar').should('have.css','background-color','rgb(204, 0, 193)')
+            })
+    
+    
+        })   
     })
-
 })
 
