25df78314dd581b73e5ddb9ce401bd9454620d56
ShlomoBluth <lomibluth@gmail.com>
2024-07-28 11:20:30 +0300

    commit

--- cypress/e2e/toolTests.js
+++ cypress/e2e/toolTests.js
@@ -51,37 +51,37 @@ urls.forEach((urlValue,urlKey)=>{
     
     
     
-            it('Run files',()=>{
-                cy.get('button').contains('Start Experiment').click({force:true})
-                cy.url().should('eq','https://generalclassify.dicta.org.il/#/experiment')
-                cy.selectUploudfile({file:'Genesis.txt'})
-                cy.waitForUploading()
-                cy.selectUploudfile({file:'Exodus.txt'})
-                cy.waitForUploading()
-                cy.get('button').contains('Analyze Classes').click({force: true})
-                cy.get(':nth-child(1) > .v-spinner > .v-clip',{timeout:1000*60}).should('not.exist')
-                cy.get('button[data-target="#text_for_classification_popup"]').click({force: true})
-                cy.selectUploudfile({
-                    file:'Leviticus.txt',
-                    popupInner:true
-                })  
-                cy.waitForUploading()           
-                cy.get('button[class="button lg bg-blue pull-right hoverable analyze"]').contains('Classify Text').click({force: true})
-                cy.get(':nth-child(9) > .bar').should('have.css','background-color','rgb(204, 0, 193)')
+            // it('Run files',()=>{
+            //     cy.get('button').contains('Start Experiment').click({force:true})
+            //     cy.url().should('eq','https://generalclassify.dicta.org.il/#/experiment')
+            //     cy.selectUploudfile({file:'Genesis.txt'})
+            //     cy.waitForUploading()
+            //     cy.selectUploudfile({file:'Exodus.txt'})
+            //     cy.waitForUploading()
+            //     cy.get('button').contains('Analyze Classes').click({force: true})
+            //     cy.get(':nth-child(1) > .v-spinner > .v-clip',{timeout:1000*60}).should('not.exist')
+            //     cy.get('button[data-target="#text_for_classification_popup"]').click({force: true})
+            //     cy.selectUploudfile({
+            //         file:'Leviticus.txt',
+            //         popupInner:true
+            //     })
+            //     cy.waitForUploading()           
+            //     cy.get('button[class="button lg bg-blue pull-right hoverable analyze"]').contains('Classify Text').click({force: true})
+            //     cy.get(':nth-child(9) > .bar').should('have.css','background-color','rgb(204, 0, 193)')
             
                 
-                // cy.get('button').contains('Analyze Classes').click({force: true})
+            //     // cy.get('button').contains('Analyze Classes').click({force: true})
     
-                // cy.get(':nth-child(1) > .v-spinner > .v-clip',{timeout:1000*60}).should('not.exist')
-                // cy.get('button[data-target="#text_for_classification_popup"]').click({force: true})
-                // cy.selectText({
-                //     collection:'Torah',
-                //     book:'Numbers',
-                //     popupInner:'true'
-                // })
-                // cy.get('button').contains('Classify Text').click({force: true})
-                // cy.get(':nth-child(13) > .bar').should('have.css','background-color','rgb(204, 0, 193)')
-            })
+            //     // cy.get(':nth-child(1) > .v-spinner > .v-clip',{timeout:1000*60}).should('not.exist')
+            //     // cy.get('button[data-target="#text_for_classification_popup"]').click({force: true})
+            //     // cy.selectText({
+            //     //     collection:'Torah',
+            //     //     book:'Numbers',
+            //     //     popupInner:'true'
+            //     // })
+            //     // cy.get('button').contains('Classify Text').click({force: true})
+            //     // cy.get(':nth-child(13) > .bar').should('have.css','background-color','rgb(204, 0, 193)')
+            // })
     
     
         })   
